62a90978f49c82f3ea9b25a51e09b18589575d95
rh16216 <rh16216@bristol.ac.uk>
2018-10-17 18:31:01 +0100

    Unrolled loop in stencil so conditional is no longer present.

--- stencil.c
+++ stencil.c
@@ -50,13 +50,30 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  tmp_image[0] = image[0] * 3.0/5.0 + image[ny] * 0.5/5.0 + image[1] * 0.5/5.0;
+  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 3.0/5.0 + image[1+(nx-1)*ny] * 0.5/5.0 + image[((nx-1)-1)*ny] * 0.5/5.0;
+  tmp_image[(ny-1)] = image[(ny-1)] * 3.0/5.0 + image[(ny-1)-1] * 0.5/5.0 + image[(ny-1)+ ny] * 0.5/5.0;
+  tmp_image[(ny-1)+(nx-1)*ny] = image[(ny-1)+(nx-1)*ny] * 3.0/5.0 + image[(ny-1)-1+(nx-1)*ny] * 0.5/5.0 + image[(ny-1) +((nx-1)-1)*ny] * 0.5/5.0;
+
+  for (int i = 1; i < nx-1; ++i) {
+    tmp_image[i*ny] = image[i*ny] * 3.0/5.0 + image[(i-1)*ny] * 0.5/5.0 + image[(i+1)*ny] * 0.5/5.0 + image[1+i*ny] * 0.5/5.0;
+  }
+  for (int i = 1; i < nx-1; ++i) {
+    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 3.0/5.0 + image[ny-1 +(i-1)*ny] * 0.5/5.0 + image[ny-1 +(i+1)*ny] * 0.5/5.0 + image[ny-2+i*ny] * 0.5/5.0;
+  }
+  for (int j = 1; j < ny-1; ++j) {
+    tmp_image[j] = image[j] * 3.0/5.0 + image[j+ny] * 0.5/5.0 + image[j-1] * 0.5/5.0 + image[j+1] * 0.5/5.0;
+  }
+  for (int j = 1; j < ny-1; ++j) {
+    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 3.0/5.0 + image[j +(nx-2)*ny] * 0.5/5.0 + image[j-1+(nx-1)*ny] * 0.5/5.0 + image[j+1+(nx-1)*ny] * 0.5/5.0;
+  }
+  for (int j = 1; j < ny-1; ++j) {
+    for (int i = 1; i < nx-1; ++i) {
       tmp_image[j+i*ny] = image[j+i*ny] * 3.0/5.0;
-      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
-      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
-      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.5/5.0;
-      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.5/5.0;
+      tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
+      tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
+      tmp_image[j+i*ny] += image[j-1+i*ny] * 0.5/5.0;
+      tmp_image[j+i*ny] += image[j+1+i*ny] * 0.5/5.0;
     }
   }
 }
