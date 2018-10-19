f0e73379dd33810f1d27aa3345ae7f18fa0dcd64
rh16216 <rh16216@bristol.ac.uk>
2018-10-19 11:17:04 +0100

    Included left and right edges in for loop.

--- stencil.c
+++ stencil.c
@@ -55,18 +55,14 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   tmp_image[(ny-1)] = image[(ny-1)] * 0.6 + image[(ny-1)-1] * 0.1 + image[(ny-1)+ ny] * 0.1;
   tmp_image[(ny-1)+(nx-1)*ny] = image[(ny-1)+(nx-1)*ny] * 0.6 + image[(ny-1)-1+(nx-1)*ny] * 0.1 + image[(ny-1) +((nx-1)-1)*ny] * 0.1;
 
-  for (int i = 1; i < nx-1; ++i) {
-    tmp_image[i*ny] = image[i*ny] * 0.6 + image[(i-1)*ny] * 0.1 + image[(i+1)*ny] * 0.1 + image[1+i*ny] * 0.1;
-    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 0.6 + image[ny-1 +(i-1)*ny] * 0.1 + image[ny-1 +(i+1)*ny] * 0.1 + image[ny-2+i*ny] * 0.1;
-  }
-
   for (int j = 1; j < ny-1; ++j) {
     tmp_image[j] = image[j] * 0.6 + image[j+ny] * 0.1 + image[j-1] * 0.1 + image[j+1] * 0.1;
     tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 0.6 + image[j +(nx-2)*ny] * 0.1 + image[j-1+(nx-1)*ny] * 0.1 + image[j+1+(nx-1)*ny] * 0.1;
   }
-  
+
   for (int i = 1; i < nx-1; ++i) {
     int row = i*ny;
+    tmp_image[row] = image[row] * 0.6 + image[(i-1)*ny] * 0.1 + image[(i+1)*ny] * 0.1 + image[1+row] * 0.1;
     for (int j = 1; j < ny-1; ++j) {
       tmp_image[j+row] = image[j+row] * 0.6;
       tmp_image[j+row] += image[j  +(i-1)*ny] * 0.1;
@@ -74,6 +70,7 @@ void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
       tmp_image[j+row] += image[j-1+row] * 0.1;
       tmp_image[j+row] += image[j+1+row] * 0.1;
     }
+    tmp_image[ny-1+row] = image[ny-1+row] * 0.6 + image[ny-1 +(i-1)*ny] * 0.1 + image[ny-1 +(i+1)*ny] * 0.1 + image[ny-2+row] * 0.1;
   }
 }
 
