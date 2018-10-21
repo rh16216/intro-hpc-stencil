08d091b2964b68e81cd9c47d795245876c3902ba
rh16216 <rh16216@bristol.ac.uk>
2018-10-21 15:25:38 +0100

    Fixed error.

--- stencil.c
+++ stencil.c
@@ -51,8 +51,8 @@ int main(int argc, char *argv[]) {
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   for (int i = 1; i < nx+1; ++i) {
     for (int j = 1; j < ny+1; ++j) {
-      tmp_image[j+i*(ny+2)] += image[j-1+i*(ny+2)] * 0.1;
       tmp_image[j+i*(ny+2)] = image[j+i*(ny+2)] * 0.6;
+      tmp_image[j+i*(ny+2)] += image[j-1+i*(ny+2)] * 0.1;
       tmp_image[j+i*(ny+2)] += image[j+1+i*(ny+2)] * 0.1;
       tmp_image[j+i*(ny+2)] += image[j+(i-1)*(ny+2)] * 0.1;
       tmp_image[j+i*(ny+2)] += image[j+(i+1)*(ny+2)] * 0.1;
